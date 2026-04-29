495e3dcc193928978c9a19b4167049df000687a3
nkk09 <nkk09@mail.aub.edu>
2026-04-29 03:40:59 +0300

    Initial commit: application code and K8s manifests

---
+++ app/server.test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+describe("Health Check", () => {
+  it("should return ok status", () => {
+    const status = "ok";
+
+    assert.strictEqual(status, "ok");
+  });
+});
